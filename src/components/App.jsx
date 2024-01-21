8e400307eadeb8d3283549b022c572bade712d83
Igor-Principal <igorprincipal@gmail.com>
2024-01-21 21:12:33 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,31 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from './Layout/Layout';
 import Home from 'Pages/Home/Home';
-// import Movies from 'Pages/Movies/Movies';
-// import MovieDetails from './MovieDetails/MovieDetails';
-// import Cast from './MovieDetails/Cast/Cast';
-// import Reviews from './MovieDetails/Reviews/Reviews';
-import { lazy } from 'react';
+import Movies from 'Pages/Movies/Movies';
+import MovieDetails from './MovieDetails/MovieDetails';
+import Cast from './MovieDetails/Cast/Cast';
+import Reviews from './MovieDetails/Reviews/Reviews';
+// import { lazy } from 'react';
 
-const Movies = lazy(() => import('Pages/Movies/Movies'));
-const MovieDetails = lazy(() => import('./MovieDetails/MovieDetails'));
-const Cast = lazy(() => import('./MovieDetails/Cast/Cast'));
-const Reviews = lazy(() => import('./MovieDetails/Reviews/Reviews'));
+// const Movies = lazy(() => import('Pages/Movies/Movies'));
+// const MovieDetails = lazy(() => import('./MovieDetails/MovieDetails'));
+// const Cast = lazy(() => import('./MovieDetails/Cast/Cast'));
+// const Reviews = lazy(() => import('./MovieDetails/Reviews/Reviews'));
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:movieId" element={<MovieDetails />}>
-          <Route path="cast" element={<Cast />} />
-          <Route path="reviews" element={<Reviews />} />
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          <Route path="*" element={<div>Error</div>} />
         </Route>
-        <Route path="*" element={<div>Error</div>} />
-      </Route>
-    </Routes>
+      </Routes>
+    </div>
   );
 };
